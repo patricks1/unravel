51cc6ee9e3008ff08b4f8ba308cfe6c0a9122a12
Eralp Sahin <eralpsahin@me.com>
2019-05-23 16:24:02 -0400

    Start tracking user diff

--- unravel.py
+++ unravel.py
@@ -1,8 +1,10 @@
+#!venv/bin/python
 '''Unravel, Piazza Deanonymizer'''
-import pprint
 import argparse
 from piazza_api import Piazza
 from tinydb import TinyDB, Query
+from jsondiff import diff
+from tinydb.operations import delete
 
 
 def get_statistics(email, password, class_id):
@@ -56,11 +58,58 @@ def parse_arguments():
 
 def main():
     args = parse_arguments()
-    # Create/load tinydb for the class
-    tinydb = TinyDB(f'{args.class_id}.json')
+
     stats = get_statistics(args.email, args.password, args.class_id)
+    stats = {'users': stats['users'],
+             'total': stats['total'], 'top': stats['top_users']}
+    # Create/load tinydb for the class
+    tinydb = TinyDB(f'{args.class_id}.json', default_table="class_stats")
     # Insert the new statistics
     tinydb.insert(stats)
+
+
+def sanitize_user(user):
+    """Sanitizes the given json by removing the following keys:
+        lti_ids, user_id, days, views
+
+    Args:
+        user: Json object for user.
+
+    Returns:
+        A sanitized JSON user object.
+
+    Raises:
+        KeyError: If any of the mentioned keys does not exist.
+    """
+    del user['lti_ids'], user['user_id'], user['days'], user['views']
+    return user
+
+
+def find_diff():
+    """Compares the previous and current class statistics.
+
+    Searches for difference in a user between two statistics records
+    in the tinydb database, prints the users with difference.
+    """
+    tinydb = TinyDB('jssylhyqghvxn.json', default_table="class_stats")
+
+    total_info = tinydb.all()[0].get('total')
+    print(f'Total in the class: {total_info}')
+
+    prev = tinydb.all()[0].get('users')
+    curr = tinydb.all()[1].get('users')
+
+    # Find a difference in a user data between two statistics records
+    found = False
+    for index, prev_user in enumerate(prev):
+        prev_user = sanitize_user(prev_user)
+        curr[index] = sanitize_user(curr[index])
+        if diff(prev_user, curr[index], syntax='explicit') != {}:
+            print(curr[index])
+            found = True
+
+    if not found:
+        print("There was no difference")
 
 
 if __name__ == '__main__':
